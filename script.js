6f7d6fdebb7bb8bd1161a35d6b2a2023f5299ad3
luciomelis <luciomelis1992@gmail.com>
2022-04-04 14:38:31 +0200

    Ho inserito la mail randomica a schermo

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ console.log('Js Ok');
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina
 // all'interno di una lista.
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        email: ''
+    },
+    methods: {
+
+    },
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+            const result = response.data;
+            console.log(result, result.response);
+            this.email = result.response;
+            console.log(this.email);
+        });
+    }
+})
